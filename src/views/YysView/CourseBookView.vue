ee324db6ccc46e2a2145c59cc71cf95a88dae8c9
seed-yoo <lukdown10@naver.com>
2024-05-13 11:23:42 +0900

    yys

--- src/views/YysView/CourseBookView.vue
+++ src/views/YysView/CourseBookView.vue
@@ -45,7 +45,7 @@
 
                   <div id="yys-searchbox">
                     <input type="text" id="yys-search-input">
-                    <button id="yys-search-btn">검색</button>
+                    <button id="yys-search-btn"><img src="@/assets/img/icon/search_3031293.png" alt=""></button>
                   </div>
 
                   <div id="yys-filterbox">
@@ -81,9 +81,11 @@
                               <div class="yys-listcontent-ex">
                                 <div class="yys-listinfo-ex1">
 
-                                    <p>
-                                      <label for="">코스 이름 :</label>
-                                      <span> 하기힘든코스</span>
+                                  <p>
+                                    <label for="">코스 이름 :</label>
+                                  </p>
+                                  <p>
+                                    <span> 하기힘든코스</span>
                                   </p>
                                   <p>
                                     <label for="">소요 시간 :</label>
@@ -121,13 +123,134 @@
 
 
 
-                      <li class="yys-list-info" @click="openModal()">
+                      <li class="yys-list-info">
+                        <div class="yys-list-contentbox">
+                          <p id="yys-course-title">
+                            <span>경기도 하남</span>
+                            <button type="button">
+                              <img src="@/assets/img/icon/star_3794158.png" alt="">
+                            </button>
+                          </p>
+
                           <div>
-                            3213123
-                          </div>
+                            <div class="yys-writing">
+                              <p id="yys-course-icon-img">
+                                <button>
+                                  <img src="@/assets/img/icon/heart_9131541.png" alt="">
+                                </button>
+                                <span>123</span>
+                                <button>
+                                  <img src="@/assets/img/icon/view_709612.png" alt="">
+                                </button>
+                                <span>123</span>
+                              </p>
+                              <div class="yys-listcontent-ex">
+                                <div class="yys-listinfo-ex1">
+
+                                  <p>
+                                    <label for="">코스 이름 :</label>
+                                  </p>
+                                  <p>
+                                    <span> 하기힘든코스</span>
+                                  </p>
+                                  <p>
+                                    <label for="">소요 시간 :</label>
+                                    <span> 20분</span>
+                                  </p>
+                                </div>
+
+
+
+                                <div class="yys-listinfo-ex2">
+                                  <p>
+                                    <label for="">난이도 :</label>
+                                    <span> 쉬움</span>
+                                  </p>
+                                  <p>
+                                    <label for="">코스길이 :</label>
+                                    <span> 1.6Km</span>
+                                  </p>
+
+                                </div>
+                              </div>
+                            </div>
+                            
+                            
+                        </div>
+                          
+                          
+                          
+                        </div>
+                        <div class="yys-list-info-arrow">
+                          <img src="@/assets/img/icon/right-arrow_3031716.png" alt="#" @click="openModal()">
+                        </div>
+                        
+                      </li>
+
+
+
+                      <li class="yys-list-info">
+                        <div class="yys-list-contentbox">
+                          <p id="yys-course-title">
+                            <span>경기도 하남</span>
+                            <button type="button">
+                              <img src="@/assets/img/icon/star_3794158.png" alt="">
+                            </button>
+                          </p>
+
                           <div>
-                              <strong>32233원</strong>
-                          </div>
+                            <div class="yys-writing">
+                              <p id="yys-course-icon-img">
+                                <button>
+                                  <img src="@/assets/img/icon/heart_9131541.png" alt="">
+                                </button>
+                                <span>123</span>
+                                <button>
+                                  <img src="@/assets/img/icon/view_709612.png" alt="">
+                                </button>
+                                <span>123</span>
+                              </p>
+                              <div class="yys-listcontent-ex">
+                                <div class="yys-listinfo-ex1">
+
+                                  <p>
+                                    <label for="">코스 이름 :</label>
+                                  </p>
+                                  <p>
+                                    <span> 하기힘든코스</span>
+                                  </p>
+                                  <p>
+                                    <label for="">소요 시간 :</label>
+                                    <span> 20분</span>
+                                  </p>
+                                </div>
+
+
+
+                                <div class="yys-listinfo-ex2">
+                                  <p>
+                                    <label for="">난이도 :</label>
+                                    <span> 쉬움</span>
+                                  </p>
+                                  <p>
+                                    <label for="">코스길이 :</label>
+                                    <span> 1.6Km</span>
+                                  </p>
+
+                                </div>
+                              </div>
+                            </div>
+                            
+                            
+                        </div>
+                          
+                          
+                          
+                        </div>
+                        <div class="yys-list-info-arrow">
+                          <img src="@/assets/img/icon/right-arrow_3031716.png" alt="#" @click="openModal()">
+                        </div>
+                        
                       </li>
                                             
                   </ul>
@@ -138,7 +261,7 @@
                 <div class="yys-reviewlist-content" v-if="isModalViewed">
 
                   <div id="yys-course-name-box">
-                    <span>코스 이름 : 산책하기 힘든 코스</span>
+                    <span>산책하기 힘든 코스</span>
                   </div>
 
                   <div id="yys-course-explanation">
@@ -151,8 +274,8 @@
                   </div>
 
                   <div id="yys-reviewbox">
-                    <input type="text" id="yys-review-input">
-                    <button id="yys-review-btn">후기등록</button>
+                    <input type="text" id="yys-review-input" placeholder="후기를 등록해주세요">
+                    <button id="yys-review-btn"><img src="@/assets/img/icon/upload_3097412.png" alt="#"></button>
                   </div>
 
 
@@ -160,18 +283,28 @@
                                             
                       <li class="yys-reviewlist-info">
                           <div>
-                            3213123
+                            <img src="@/assets/img/icon/people_11081625.png" alt="#">
                           </div>
-                          <div>
-                              <strong>32233원</strong>
+                          <div id="yys-explanation">
+                              <span>이 코스 짱 어려움</span>
                           </div>
                       </li>
+
                       <li class="yys-reviewlist-info">
                           <div>
-                            3213123
+                            <img src="@/assets/img/icon/people_11081625.png" alt="#">
                           </div>
+                          <div id="yys-explanation">
+                              <span>이 코스 짱 어려움</span>
+                          </div>
+                      </li>
+
+                      <li class="yys-reviewlist-info">
                           <div>
-                              <strong>32233원</strong>
+                            <img src="@/assets/img/icon/people_11081625.png" alt="#">
+                          </div>
+                          <div id="yys-explanation">
+                              <span>이 코스 짱 어려움</span>
                           </div>
                       </li>
                                                                   
