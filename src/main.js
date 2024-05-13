eea42799b7da94581fa16b06936e1ecbc8088baa
seed-yoo <lukdown10@naver.com>
2024-05-13 21:57:58 +0900

    yys

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useKakao } from 'vue3-kakao-maps/@utils';
 
+//캘린더
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
 
 useKakao('df6af04d0c7740cc52da078913f38627', ['clusterer', 'services', 'drawing']);   // api-key입력(javascript-key)
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).component('VueDatePicker', VueDatePicker).use(store).use(router).mount('#app')
 window.Kakao.init('bec95067442add30fba8a7cedcf638ff')
--- src/views/YysView/CalenderView.vue
+++ src/views/YysView/CalenderView.vue
@@ -1,151 +1,131 @@
 <template>
-    <div>
-      <AppHeader />
-  
-  
-  
-  
-      <div id="wrap">
-        <div id="container" >
-          <!-- aside 없음 -->
-          <div id="full-content">
-            <!-- content-head 없음 -->
-  
-            <div id="index">
-  
-  
-              <div id="greetings">
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-                <div class="yys-title">
-                  <h2>코스북</h2>
-                </div>
-                <div id="yys-contentbox">
+  <div>
+    <AppHeader />
 
+    <div id="wrap">
+      <div id="container">
+        <!-- aside 없음 -->
+        <div id="full-content">
+          <!-- content-head 없음 -->
 
-                  
+          <div id="index">
+            <div id="greetings">
 
 
 
 
 
 
-
-                  <div class="tabmenu">
-                    <ul>
-                      <li id="tab1" class="btnCon">
-                        <a class="yys-calender-category-btn" href="#tab1">
-                          캘린더
-                        </a>
-                        <div class="tabCon" >
-                          dasds
-                        </div>
-                        
-                      </li>
-
-                      <li id="tab2" class="btnCon">
-                        <a class="yys-calender-category-btn" href="#tab2">
-                          기록
-                        </a>
-                        <div class="tabCon" >
-                          fdsf
-                        </div>
-                        
-                      </li>    
-                      
-                    </ul>
-                  </div>
-
-
-
-
-
-
-
-                  
-                  
-                  
-                
-  
-  
-  
-  
-  
-  
-  
-                </div>
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+              <div class="yys-calender-title">
+                <h2>나의 산책</h2>
               </div>
-  
-  
-  
-              <!-- //greetings -->
-              <div class="clear"></div>
+
+              <div id="yys-tabmenu">
+                <div class="tabmenu">
+                  <ul>
+                    <li id="tab1" class="btnCon">
+                      <a class="yys-calender-category-btn" href="#tab1">
+                        캘린더
+                      </a>
+                      <div class="tabCon">
+                        <FullCalendar :options="calendarOptions" id="calender" />
+                      </div>
+                    </li>
+
+                    <li id="tab2" class="btnCon">
+                      <a class="yys-calender-category-btn" href="#tab2">
+                        기록
+                      </a>
+                      <div class="tabCon">기록 내용 여기다 쓰세요</div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+
+
+
+
+
+
+
             </div>
-            <!-- //index -->
+
+            <!-- //greetings -->
+            <div class="clear"></div>
           </div>
-          <!-- //full-content -->
+          <!-- //index -->
         </div>
-        <!-- //container -->
-  
+        <!-- //full-content -->
       </div>
-      <!-- //wrap -->
-      
-      
-      
-      
-      
-      <AppFooter />
-      <!-- //footer -->
+      <!-- //container -->
     </div>
-  </template>
+    <!-- //wrap -->
+
+    <AppFooter />
+    <!-- //footer -->
+  </div>
+</template>
   
   
   
   
-  <script>
-  
-  import "@/assets/css/YysCss/CalenderView.css";
-  import AppFooter from "@/components/AppFooter.vue";
-  import AppHeader from "@/components/AppHeader.vue";
-  
-  location.href = "#tab1";
-  export default {
-    name: 'CalenderView',
-    components: {
-      AppFooter,
-      AppHeader,
-    },
-    data() {
-        return {
-        };
-    },
-    methods: {
-    },
-    created(){
-  
-    }
-  }
-  </script>
+<script>
+import "@/assets/css/YysCss/CalenderView.css";
+import AppFooter from "@/components/AppFooter.vue";
+import AppHeader from "@/components/AppHeader.vue";
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import Swal from "sweetalert2";
+
+location.href = "#tab1";
+
+export default {
+  name: "CalenderView",
+  components: {
+    AppFooter,
+    AppHeader,
+    FullCalendar,
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: "dayGridMonth",
+        headerToolbar: {
+          // 헤더에 표시할 툴 바
+          start: "prev next today",
+          center: "title",
+          end: "dayGridMonth,dayGridWeek,dayGridDay",
+        },
+        contentHeight: 1000,
+        weekend: true,
+        locale: "ko",
+        events: [
+          { title: "Meeting", start: new Date() },
+          {
+            title: "Meeting",
+            start: "2024-05-10 08:00:00",
+            end: "2024-05-12 01:00:00",
+          },
+          { title: "Meeting", start: "2024-05-22 18:00:00", end: "2024-05-23 01:00:00" },
+        ],
+        eventClick: function (info) {
+          Swal.fire({
+            title: "일정",
+            html:
+              "스케줄: " + info.event.title +
+              "<br/>일시: " + new Date(info.event.start).toLocaleString().substring(0, 20).replace("/g", ""),
+          });
+          //alert('스케줄: ' + info.event.title + '\n' + '일시: ' + (info.event.start));
+
+          // change the border color just for fun
+        },
+      },
+    };
+  },
+  methods: {},
+  created() {},
+};
+</script>
   
