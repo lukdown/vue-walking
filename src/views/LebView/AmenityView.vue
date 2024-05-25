795a07c7072d6b451ea4371afa80d7030e258348
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-05-25 18:19:00 +0900

    qwg

--- src/views/LebView/AmenityView.vue
+++ src/views/LebView/AmenityView.vue
@@ -53,13 +53,15 @@ export default {
   },
   data() {
     return {
-      convenient_facilities_list:[],
+      convenient_facilities_list: [],
+      Facilities_For_The_Disabled_List:[],
       map: null,
     }
   },
   mounted() {
     if (window.kakao && window.kakao.maps) {
-      this.initMap();
+      this.loadMapScript();
+
     } else {
       const script = document.createElement("script");
       /* global kakao */
@@ -71,9 +73,9 @@ export default {
 
   },
   methods: {
-    list(){
+    async list() {
       console.log("리스트 나와랑");
-      axios({
+      await axios({
         method: "get", // put, post, delete
         url: `${this.$store.state.apiBaseUrl}/api/walking/convenientlist`,
         headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
@@ -84,75 +86,86 @@ export default {
       })
         .then((response) => {
           console.log(response); //수신데이타
-          this.coursebookList = response.data;
+          this.convenient_facilities_list = response.data.apiData;
+          this.initMap();
+          console.log(this.convenient_facilities_list);
         })
         .catch((error) => {
           console.log(error);
         });
     },
+    FacilitiesForTheDisabledList() {
+      console.log("장애인리스트 나와랑");
+      axios({
+        method: "get", // put, post, delete
+        url: `http://api.odcloud.kr/api/15075529/v1/uddi:ed5cbc13-b65a-49ac-b83d-cefcf9806fd3?page=1&perPage=266&returnType=json&serviceKey=I7auO7GQYlyZTrhZho8s6cvevXCz8AmynuTV9vPjdaqlO%2B3dkXjc1Hu1ABDhgXPULJW2GGwQmg74YlbG85MsZQ%3D%3D`,
+        headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+        //params: course_category_no, //get방식 파라미터로 값이 전달
+        //data: course_category_no, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+        responseType: "json", //수신타입
+      })
+        .then((response) => {
+          console.log(response); //수신데이타
+          this.Facilities_For_The_Disabled_List = response.data.data;
+          console.log(this.Facilities_For_The_Disabled_List);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    loadMapScript() {
+      this.list();
+    },
     initMap() {
-      var mapContainer = document.getElementById('pjhmap'), // 지도를 표시할 div  
-        mapOption = {
-          center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-          level: 3 // 지도의 확대 레벨
-        };
+      if (this.convenient_facilities_list.length === 0) return;
+
+      const mapContainer = document.getElementById('pjhmap'); // 지도를 표시할 div  
+      const mapOption = {
+        center: new kakao.maps.LatLng(37.5286, 127.1264), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+      };
 
       var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
       // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
-      var positions = [
-        {
-          content: '<div>카카오</div>',
-          latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-        },
-        {
-          content: '<div>생태연못</div>',
-          latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-        },
-        {
-          content: '<div>텃밭</div>',
-          latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-        },
-        {
-          content: '<div>근린공원</div>',
-          latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-        }
-      ];
+      this.convenient_facilities_list.forEach((facility) => {
+        const position = new kakao.maps.LatLng(facility.facilities_latitude, facility.facilities_longitude);
 
-      for (var i = 0; i < positions.length; i++) {
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-          map: map, // 마커를 표시할 지도
-          position: positions[i].latlng // 마커의 위치
+        // 마커 이미지 설정
+        const imageSrc = facility.convenient_facilities_type_no == 1 ?  require('@/assets/img/toiletmarker.png') :
+                         facility.convenient_facilities_type_no == 2 ? '@/assets/img/yellowping.png' :
+                         '@/assets/img/yellowping.png';
+
+        const imageSize = new kakao.maps.Size(38, 38);
+        const imageOption = { offset: new kakao.maps.Point(38, 38) };
+
+        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position,
+          image: markerImage
         });
 
-        // 마커에 표시할 인포윈도우를 생성합니다 
-        var infowindow = new kakao.maps.InfoWindow({
-          content: positions[i].content // 인포윈도우에 표시할 내용
+        const infowindow = new kakao.maps.InfoWindow({
+          content: facility.facilities_name,
         });
 
-        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-        // 이벤트 리스너로는 클로저를 만들어 등록합니다 
-        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-        kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-      }
+        kakao.maps.event.addListener(marker, 'mouseover', () => infowindow.open(map, marker));
+        kakao.maps.event.addListener(marker, 'mouseout', () => infowindow.close());
+      });
 
-      // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-      function makeOverListener(map, marker, infowindow) {
-        return function () {
-          infowindow.open(map, marker);
-        };
-      }
 
-      // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
-      function makeOutListener(infowindow) {
-        return function () {
-          infowindow.close();
-        };
-      }
+
+
     },
-    
+
+  },
+  created() {
+    this.list();
+    this.FacilitiesForTheDisabledList();
   }
 
 
