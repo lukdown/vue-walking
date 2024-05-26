8a74e7828450c863d83df5195c8bdfdfcaddf844
pjh990909 <pjh990909@naver.com>
2024-05-26 23:36:34 +0900

    ㅂㅈㅎ

--- src/views/LebView/AmenityView.vue
+++ src/views/LebView/AmenityView.vue
@@ -31,7 +31,24 @@
       </div>
       <!--오른쪽 지도-->
       <div id="leb-amenity-right-map">
-        <div id="pjhmap"></div>
+        <div id="pjhmap">
+          <div class="category">
+            <ul>
+              <li id="coffeeMenu" onclick="changeMarker('coffee')">
+                <span class="ico_toilet"></span>
+                <span class="ico_font">화장실</span>
+              </li>
+              <li id="storeMenu" onclick="changeMarker('store')">
+                <span class="ico_store"></span>
+                <span class="ico_font">장애복지관</span>
+              </li>
+              <li id="carparkMenu" onclick="changeMarker('carpark')">
+                <span class="ico_carpark"></span>
+                <span class="ico_font">주차장</span>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
     <AppFooter />
@@ -54,7 +71,7 @@ export default {
   data() {
     return {
       convenient_facilities_list: [],
-      Facilities_For_The_Disabled_List:[],
+      Facilities_For_The_Disabled_List: [],
       map: null,
     }
   },
@@ -94,11 +111,13 @@ export default {
           console.log(error);
         });
     },
+    
     FacilitiesForTheDisabledList() {
+      const self = this;
       console.log("장애인리스트 나와랑");
       axios({
         method: "get", // put, post, delete
-        url: `http://api.odcloud.kr/api/15075529/v1/uddi:ed5cbc13-b65a-49ac-b83d-cefcf9806fd3?page=1&perPage=266&returnType=json&serviceKey=I7auO7GQYlyZTrhZho8s6cvevXCz8AmynuTV9vPjdaqlO%2B3dkXjc1Hu1ABDhgXPULJW2GGwQmg74YlbG85MsZQ%3D%3D`,
+        url: `https://api.odcloud.kr/api/15075529/v1/uddi:ed5cbc13-b65a-49ac-b83d-cefcf9806fd3?page=1&perPage=254&returnType=json&serviceKey=I7auO7GQYlyZTrhZho8s6cvevXCz8AmynuTV9vPjdaqlO%2B3dkXjc1Hu1ABDhgXPULJW2GGwQmg74YlbG85MsZQ%3D%3D`,
         headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
         //params: course_category_no, //get방식 파라미터로 값이 전달
         //data: course_category_no, //put, post, delete 방식 자동으로 JSON으로 변환 전달
@@ -109,11 +128,53 @@ export default {
           console.log(response); //수신데이타
           this.Facilities_For_The_Disabled_List = response.data.data;
           console.log(this.Facilities_For_The_Disabled_List);
+
+          for (let i = 1; i <= self.Facilities_For_The_Disabled_List.length; i++) {
+            axios.get(`${self.$store.state.apiBaseUrl}/api/walking/facilitieslistcomparison/` + self.Facilities_For_The_Disabled_List[i].시설명)
+              .then(function (res) {
+                console.log(res);
+                if (res.status == 200) {
+                  console.log(res.data.apiData);
+                  if (res.data.apiData == false) {
+                    console.log("등록");
+                    //self.FacilitiesForTheDisabledListInsert();
+                    console.log(self.Facilities_For_The_Disabled_List[i]["와이(Y)좌표"]);
+                    console.log(self.Facilities_For_The_Disabled_List[i]["엑스(X)좌표"]);
+                    console.log(self.Facilities_For_The_Disabled_List[i].시설명);
+
+                    axios({
+                      method: "post", // put, post, delete
+                      url: `${self.$store.state.apiBaseUrl}/api/walking/facilitieslistcomparisoninsert/` + self.Facilities_For_The_Disabled_List[i].시설명 + "/" + self.Facilities_For_The_Disabled_List[i]["와이(Y)좌표"] + "/" + self.Facilities_For_The_Disabled_List[i]["엑스(X)좌표"] + "/" + self.Facilities_For_The_Disabled_List[i].전화번호,
+                      headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                      //params: course_category_no, //get방식 파라미터로 값이 전달
+                      //data: self.Facilities_For_The_Disabled_List[i], //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+                      responseType: "json", //수신타입
+                    })
+                      .then((response) => {
+                        console.log(response); //수신데이타
+
+                      })
+                      .catch((error) => {
+                        console.log(error);
+                      });
+
+
+                  } else {
+                    console.log("스타워킹");
+                  }
+                }
+
+              })
+          }
+
         })
         .catch((error) => {
           console.log(error);
         });
+
     },
+
     loadMapScript() {
       this.list();
     },
@@ -133,9 +194,9 @@ export default {
         const position = new kakao.maps.LatLng(facility.facilities_latitude, facility.facilities_longitude);
 
         // 마커 이미지 설정
-        const imageSrc = facility.convenient_facilities_type_no == 1 ?  require('@/assets/img/toiletmarker.png') :
-                         facility.convenient_facilities_type_no == 2 ? '@/assets/img/yellowping.png' :
-                         '@/assets/img/yellowping.png';
+        const imageSrc = facility.convenient_facilities_type_no == 1 ? require('@/assets/img/toiletmarker.png') :
+          facility.convenient_facilities_type_no == 2 ? require('@/assets/img/Disabledmarker.png') :
+            '@/assets/img/yellowping.png';
 
         const imageSize = new kakao.maps.Size(38, 38);
         const imageOption = { offset: new kakao.maps.Point(38, 38) };
@@ -177,4 +238,6 @@ export default {
   width: 1180px;
   height: 703px;
 }
+
+
 </style>
