fcd5d754e695b4c8a6662d7768058af1d2c74856
seed-yoo <lukdown10@naver.com>
2024-05-28 14:53:45 +0900

    yys

--- src/components/YysKakaoMap/CoursebookKakaoMap.vue
+++ src/components/YysKakaoMap/CoursebookKakaoMap.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div id="map">
-        <div style="display: none;">course번호 TEST : {{ this.course_no = childVaule }}</div>
+      <div style="display: none">
+        course번호 TEST : {{ (this.course_no = childVaule) }}
+      </div>
     </div>
   </div>
 </template>
@@ -14,17 +16,17 @@ export default {
   props: ["childVaule"],
   data() {
     return {
+      course_no: "",
+      course_point_Vo: {
+        course_point_no: "",
         course_no: "",
-        course_point_Vo: {
-            course_point_no: "",
-            course_no: "",
-            course_latitude: "",
-            course_longitude: "",
-            course_order: "",
-            course_division: "",
-            group_num: "",
-        },
-        course_point_List: [],
+        course_latitude: "",
+        course_longitude: "",
+        course_order: "",
+        course_division: "",
+        group_num: "",
+      },
+      course_point_List: [],
     };
   },
   mounted() {
@@ -42,7 +44,6 @@ export default {
   },
   methods: {
     initMap(course_no) {
-      
       console.log("코스 번호 가져오기");
       console.log(course_no);
       this.course_point_Vo.course_no = course_no;
@@ -62,94 +63,111 @@ export default {
           this.course_point_List = response.data.apiData;
           console.log(this.course_point_List);
 
+          // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
+          var markers = [];
+          var linePath = [];
           
-      var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-        mapOption = {
-          center: new kakao.maps.LatLng(37.498457376358886, 127.02681299738605), // 지도의 중심좌표
-          level: 3, // 지도의 확대 레벨
-        };
+          var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+            mapOption = {
+              center: new kakao.maps.LatLng(
+                37.498457376358886,
+                127.02681299738605
+              ), // 지도의 중심좌표
+              level: 3, // 지도의 확대 레벨
+            };
+
+          var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+          // 마커 하나를 지도위에 표시합니다
+
+          for (let index = 0; index < this.course_point_List.length; index++) {
 
 
-        
-        
+            addMarker(
+              new kakao.maps.LatLng(
+                this.course_point_List[index].course_latitude,
+                this.course_point_List[index].course_longitude
+              )
+            );
+            linePath.push(
+              new kakao.maps.LatLng(
+                this.course_point_List[index].course_latitude,
+                this.course_point_List[index].course_longitude
+              )
+            );
 
-      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+            var moveLatLon = new kakao.maps.LatLng(
+                this.course_point_List[0].course_latitude,
+                this.course_point_List[0].course_longitude
+              )
 
-      
+              map.setCenter(moveLatLon);
+          }
+          
 
-      // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
-      var markers = [];
-      var linePath = [];
-
-      // 마커 하나를 지도위에 표시합니다 
-
-        for (let index = 0; index < this.course_point_List.length; index++) {
-          addMarker(new kakao.maps.LatLng(this.course_point_List[index].course_latitude, this.course_point_List[index].course_longitude));
-
-        }
-        for (let index = 0; index < this.course_point_List.length; index++) {
-          //console.log(this.course_point_List[index].course_order);
-          linePath.push(new kakao.maps.LatLng(this.course_point_List[index].course_latitude, this.course_point_List[index].course_longitude));
-        }
-
-        
-
-        // 지도에 표시할 선을 생성합니다
-        var polyline = new kakao.maps.Polyline({
+          
+          // 지도에 표시할 선을 생성합니다
+          var polyline = new kakao.maps.Polyline({
             path: linePath, // 선을 구성하는 좌표배열 입니다
             strokeWeight: 5, // 선의 두께 입니다
-            strokeColor: 'blue', // 선의 색깔입니다
+            strokeColor: "blue", // 선의 색깔입니다
             strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: 'solid' // 선의 스타일입니다
-        });
-
-        // 지도에 선을 표시합니다 
-        polyline.setMap(map); 
-
-
-
-
-
-
-
-
-        
-        console.log(this.course_point_Vo.course);
-      // 마커를 생성하고 지도위에 표시하는 함수입니다
-      function addMarker(position) {
-          
-          // 마커를 생성합니다
-          var marker = new kakao.maps.Marker({
-              position: position
+            strokeStyle: "solid", // 선의 스타일입니다
           });
 
-          // 마커가 지도 위에 표시되도록 설정합니다
-          marker.setMap(map);
-          
-          
-          // 생성된 마커를 배열에 추가합니다
-          markers.push(marker);
-          
-         
 
-      }
-      
-          
+
+
+
+
+
+
+
+
+
+
+
+
+
+          // 지도에 선을 표시합니다
+          polyline.setMap(map);
+
+          //console.log(this.course_point_Vo.course);
+          // 마커를 생성하고 지도위에 표시하는 함수입니다
+          function addMarker(position) {
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+              position: position,
+            });
+
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(map);
+
+            // 생성된 마커를 배열에 추가합니다
+            markers.push(marker);
+          }
+
+
+
+
+
+
+
+
+
+
+
+
 
         })
         .catch((error) => {
           console.log(error);
         });
-
-
-        
-
     },
   },
   created() {
     //console.log(this.course_no);
     //this.getpointList()
-    
   },
 };
 </script>
