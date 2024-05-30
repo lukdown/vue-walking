cfff947bbdcf605d16a4fefdc1da79b8bd924b21
seed-yoo <lukdown10@naver.com>
2024-05-30 20:27:26 +0900

    yys

--- src/views/KsbView/AchievementView.vue
+++ src/views/KsbView/AchievementView.vue
@@ -1,119 +1,116 @@
 <template>
-    <div>
-        <AppHeader/>
-        <div id="ksb-achieve-All">
-            <div id="ksb-achieve-subTitle">
-                <h2>도전과제</h2>
-                <span>달성완료 20/100 개</span>
-            </div>
-            <div id="ksb-achieve-bookArea">
-                <ul>
-                    <li>
-                        <div class="ksb-achieve-List">
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>초보자</span>
-                                        <p>처음으로 산책을 시작했습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>우왕~ 많이 걸었당~</span>
-                                        <p>누적 50Km를 걸었습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>영 수</span>
-                                        <p>이틀 연속으로 산책을 나갔습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>프로산책러</span>
-                                        <p>누적 500Km를 걸었습니다.</p>
-                                    </div>
-                                </div>
-                                <button id="ksb-prev-btn">이전</button>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="ksb-achieve-List">
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>초보자</span>
-                                        <p>처음으로 산책을 시작했습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>우왕~ 많이 걸었당~</span>
-                                        <p>누적 50Km를 걸었습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>영 수</span>
-                                        <p>이틀 연속으로 산책을 나갔습니다.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div id="ksb-achieve-imgArea">
-                                    <img src="" alt="" class="ksb-achieve-Img">
-                                    <div class="ksb-achieve-text">
-                                        <span>프로산책러</span>
-                                        <p>누적 500Km를 걸었습니다.</p>
-                                    </div>
-                                </div>
-                                <button id="ksb-next-btn">다음</button>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+  <div>
+    <AppHeader />
+    <div id="ksb-achieve-All">
+      <div id="yys-box">
+        <div id="ksb-achieve-subTitle">
+          <h2>도전과제</h2>
+          <span>{{ currentPage }} / {{ totalPages }}</span>
+          <span>달성완료 3/9 개</span>
         </div>
-        <AppFooter/>
+        <div id="yys-box2">
+          <div id="ksb-achieve-bookArea">
+            
+            <div id="yys-achieve-List-box">
+                <ul class="ksb-achieve-List">
+                  <li id="ksb-achieve-imgArea" v-for="item in leftColumn" :key="item.id">
+                    <div class="yys-achieve-img">
+                      <img src="@/assets/img/치킨.jpg" alt="" class="ksb-achieve-Img" />
+                    </div>
+                    <div class="ksb-achieve-text">
+                      <p>{{ item.challenge_name }}</p>
+                      <span>{{ item.challenge_explanation }}</span>
+                    </div>
+                  </li>
+                </ul>
+                <ul class="ksb-achieve-List">
+                  <li id="ksb-achieve-imgArea" v-for="item in rightColumn" :key="item.id">
+                    <div class="yys-achieve-img">
+                      <img src="@/assets/img/치킨.jpg" alt="" class="ksb-achieve-Img" />
+                    </div>
+                    <div class="ksb-achieve-text">
+                      <p>{{ item.challenge_name }}</p>
+                      <span>{{ item.challenge_explanation }}</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+
+                <div id="yys-paging-btn">
+                  <div class="yys-btn">
+                    <button class="yys-arrow-img" v-if="currentPage > 1" @click="currentPage--">이전</button>
+                  </div>
+                  <span id="yys-btn-center"></span>
+                  <div class="yys-btn">
+                    <button class="yys-arrow-img" v-if="currentPage < totalPages" @click="currentPage++">다음</button>
+                  </div>
+                </div>
+              </div>
+        </div>
+      </div>
     </div>
+    <AppFooter />
+  </div>
 </template>
 
 
 <script>
+import axios from "axios";
 import "@/assets/css/KsbCss/Achievement.css";
 import AppFooter from "@/components/AppFooter.vue";
 import AppHeader from "@/components/AppHeader.vue";
+
 export default {
-    name: "AchievementView",
-    components: {
-        AppFooter,
-        AppHeader,
+  name: "AchievementView",
+  components: {
+    AppFooter,
+    AppHeader,
+  },
+  data() {
+    return {
+      challengeList: [],
+      currentPage: 1, // 현재 페이지 번호
+      postsPerPage: 6, // 한 페이지에 보여줄 게시글의 개수
+    };
+  },
+  computed: {
+    displayedPosts() {
+      const startIndex = (this.currentPage - 1) * this.postsPerPage;
+      const endIndex = startIndex + this.postsPerPage;
+      return this.challengeList.slice(startIndex, endIndex);
     },
-    data() {
-        return {};
+    leftColumn() {
+      return this.displayedPosts.slice(0, 3);
     },
-    methods: {},
+    rightColumn() {
+      return this.displayedPosts.slice(3);
+    },
+    totalPages() {
+      return Math.ceil(this.challengeList.length / this.postsPerPage);
+    },
+  },
+  methods: {
+    getchallengeList() {
+      console.log("데이터 가져오기");
+
+      axios({
+        method: "get", 
+        url: `${this.$store.state.apiBaseUrl}/api/walking/mypage_challenge_list`,
+        headers: { "Content-Type": "application/json; charset=utf-8" }, 
+        responseType: "json", 
+      })
+        .then((response) => {
+          console.log(response); 
+          this.challengeList = response.data.apiData;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getchallengeList();
+  },
 };
 </script>
 
-
-<style></style>
--- src/views/YysView/YysTestView.vue
+++ src/views/YysView/YysTestView.vue
@@ -1,105 +1,119 @@
 <template>
-    <quill-editor
-      v-model:value="state.content"
-      :options="state.editorOption"
-      @change="onEditorChange($event)"
-    ></quill-editor>
-    <div>
-      <button
-        style="
-          border: none;
-          border-radius: 7px;
-          width: 100px;
-          height: 35px;
-          font-size: 23px;
-          margin-top: 30px;
-          background-color: rgb(209, 209, 209);
-          color: #2c3e50;
-        "
-        @click="submit(state, title)"
-      >
-        올리기
-      </button>
-    </div>
-  </template>
-  
-  <script>
-  import { reactive } from 'vue'
-  
-  export default {
-    name: "YysTestView",
-    setup() {
-      const state = reactive({
-        content: '',
-        _content: '',
-        editorOption: {
-          placeholder: '내용을 입력해주세요...', // placeholder 설정
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              ['blockquote', 'code-block'],
-              [{ header: 1 }, { header: 2 }],
-              [{ list: 'ordered' }, { list: 'bullet' }],
-              [{ script: 'sub' }, { script: 'super' }],
-              [{ indent: '-1' }, { indent: '+1' }],
-              [{ direction: 'rtl' }],
-              [{ size: ['small', false, 'large', 'huge'] }],
-              [{ header: [1, 2, 3, 4, 5, 6, false] }],
-              [{ color: [] }, { background: [] }],
-              [{ font: [] }],
-              [{ align: [] }],
-              ['clean'],
-              ['link', 'image', 'video']
-            ]
-          }
-          // more options
-        },
-        disabled: false
-      })
-  
-      const onEditorBlur = (quill) => {
-        console.log('editor blur!', quill)
-      }
-      const onEditorFocus = (quill) => {
-        console.log('editor focus!', quill)
-      }
-      const onEditorReady = (quill) => {
-        console.log('editor ready!', quill)
-      }
-      const onEditorChange = ({ quill, html, text }) => {
-        console.log('editor change!', quill, html, text)
-        state._content = html
-      }
-  
-      setTimeout(() => {
-        state.disabled = true
-      }, 2000)
-  
-      return { state, onEditorBlur, onEditorFocus, onEditorReady, onEditorChange }
-    },
-    props: { title: String },
-    data() {
-      return {}
-    },
-    methods: {
-      submit(state, title) {
-        console.log(state._content)
-        console.log(title)
-      }
-    }
-  }
-  </script>
-  <style>
-  .ql-toolbar {
-    width: 800px !important;
-  }
-  .ql-container {
-    width: 800px !important;
-    height: 500px;
-  }
-  .ql-editor {
-    height: 500px;
-    overflow: scroll;
-    overflow-x: hidden;
-  }
-  </style>
+  <div>
+      <AppHeader/>
+      <div id="ksb-achieve-All">
+          <div id="ksb-achieve-subTitle">
+              <h2>도전과제</h2>
+              <span>달성완료 20/100 개</span>
+          </div>
+          <div id="ksb-achieve-bookArea">
+              <ul>
+                  <li>
+                      <div class="ksb-achieve-List">
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>초보자</span>
+                                      <p>처음으로 산책을 시작했습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>우왕~ 많이 걸었당~</span>
+                                      <p>누적 50Km를 걸었습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>영 수</span>
+                                      <p>이틀 연속으로 산책을 나갔습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>프로산책러</span>
+                                      <p>누적 500Km를 걸었습니다.</p>
+                                  </div>
+                              </div>
+                              <button id="ksb-prev-btn">이전</button>
+                          </div>
+                      </div>
+                  </li>
+                  <li>
+                      <div class="ksb-achieve-List">
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>초보자</span>
+                                      <p>처음으로 산책을 시작했습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>우왕~ 많이 걸었당~</span>
+                                      <p>누적 50Km를 걸었습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>영 수</span>
+                                      <p>이틀 연속으로 산책을 나갔습니다.</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                              <div id="ksb-achieve-imgArea">
+                                  <img src="" alt="" class="ksb-achieve-Img">
+                                  <div class="ksb-achieve-text">
+                                      <span>프로산책러</span>
+                                      <p>누적 500Km를 걸었습니다.</p>
+                                  </div>
+                              </div>
+                              <button id="ksb-next-btn">다음</button>
+                          </div>
+                      </div>
+                  </li>
+              </ul>
+          </div>
+      </div>
+      <AppFooter/>
+  </div>
+</template>
+
+
+<script>
+import "@/assets/css/KsbCss/Achievement.css";
+import AppFooter from "@/components/AppFooter.vue";
+import AppHeader from "@/components/AppHeader.vue";
+export default {
+  name: "AchievementView",
+  components: {
+      AppFooter,
+      AppHeader,
+  },
+  data() {
+      return {};
+  },
+  methods: {},
+};
+</script>
+
+
+<style></style>
