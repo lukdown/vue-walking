8551e14abc31ac425148430ef0b574085e78954e
seed-yoo <lukdown10@naver.com>
2024-06-07 11:54:13 +0900

    yys

--- src/views/LebView/SmallGatheringDetailView.vue
+++ src/views/LebView/SmallGatheringDetailView.vue
@@ -3,90 +3,152 @@
     <AppHeader />
     <div class="leb-background">
       <div id="leb-smallgathering-detail">
-
-        <div v-if="this.$store.state.authUser.users_no == this.gatheringVo.users_no"
-          id="leb-smallgathering-detail-writer-buttons">
+        <div
+          v-if="
+            this.$store.state.authUser.users_no == this.gatheringVo.users_no
+          "
+          id="leb-smallgathering-detail-writer-buttons"
+        >
           <h2>소모임</h2>
           <div>
             <button id="leb-smallgathering-detail-modify-button">
-              <router-link id="leb-smallgathering-detail-router-link"
-                v-bind:to="`/walking/smallgatheringmodify/${gatheringVo.small_gathering_no}`" >수정</router-link>
-              </button>
-            <button id="leb-smallgathering-detail-delete-button">삭제</button>
+              <router-link
+                id="leb-smallgathering-detail-router-link"
+                v-bind:to="`/walking/smallgatheringmodify/${gatheringVo.small_gathering_no}`"
+                >수정</router-link
+              >
+            </button>
+            <button
+              id="leb-smallgathering-detail-delete-button"
+              @click="SmallGatheringDelete()"
+            >
+              삭제
+            </button>
           </div>
-
-
-
         </div>
-        <div v-else-if="this.$store.state.authUser.users_no != this.gatheringVo.users_no"
-          id="leb-smallgathering-detail-applicant-buttons">
+        <div
+          v-else-if="
+            this.$store.state.authUser.users_no != this.gatheringVo.users_no
+          "
+          id="leb-smallgathering-detail-applicant-buttons"
+        >
           <h2>소모임</h2>
-          <div>
-            <button id="leb-smallgathering-detail-apply-button">신청하기</button>
+          <div v-if="this.gatheringVo.is_application == 0">
+              <button id="leb-smallgathering-detail-apply-button" @click="applicationUpdate()">
+                신청하기
+              </button>
           </div>
-
-
+          <div v-else-if="this.gatheringVo.is_application == 1">
+              <button id="leb-smallgathering-detail-apply-button" @click="applicationDelete()">
+                신청취소
+              </button>
+            </div>
         </div>
         <!--사진등록-->
         <div id="leb-smallgathering-detail-img">
-          <img v-bind:src="`${this.$store.state.apiBaseUrl}/upload/${gatheringVo.small_gathering_saveName}`"
-            alt="미리보기 이미지" id="leb-preview">
+          <img
+            v-bind:src="`${this.$store.state.apiBaseUrl}/upload/${gatheringVo.small_gathering_saveName}`"
+            alt="미리보기 이미지"
+            id="leb-preview"
+          />
         </div>
 
         <!--소모임 이름-->
-        <div id="leb-smallgathering-detail-name" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-name"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">소모임 이름</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_name }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.small_gathering_name
+          }}</span>
         </div>
 
         <!--주최자 이름-->
-        <div id="leb-smallgathering-detail-host-name" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-host-name"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">주최자 이름</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_host_name }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.small_gathering_host_name
+          }}</span>
         </div>
 
         <!--주최자 연락처-->
-        <div id="leb-smallgathering-detail-host-number" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-host-number"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">주최자 연락처</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_hp }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.small_gathering_hp
+          }}</span>
         </div>
 
         <!--모집인원-->
-        <div id="leb-smallgathering-detail-required" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-required"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">모집인원</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.application_no_count }}명 / {{ gatheringVo.small_gathering_total_personnel }}명</span>
+          <span class="leb-smallgathering-detail-content"
+            >{{ gatheringVo.application_no_count }}명 /
+            {{ gatheringVo.small_gathering_total_personnel }}명</span
+          >
         </div>
 
         <!--코스-->
 
-        <div id="leb-smallgathering-detail-course-choice" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-course-choice"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">코스 이름</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.course_name }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.course_name
+          }}</span>
         </div>
 
-
         <!--모임 일시-->
-        <div id="leb-smallgathering-detail-date" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-date"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">모임 일시</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_date }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.small_gathering_date
+          }}</span>
         </div>
 
         <!--신청 마감일-->
-        <div id="leb-smallgathering-detail-closing-date" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-closing-date"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span class="leb-smallgathering-detail-title">신청마감일</span>
-          <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_deadline }}</span>
+          <span class="leb-smallgathering-detail-content">{{
+            gatheringVo.small_gathering_deadline
+          }}</span>
         </div>
 
         <!--신청제한-->
-        <div id="leb-smallgathering-detail-limit" class="leb-smallgathering-detail-img-right">
+        <div
+          id="leb-smallgathering-detail-limit"
+          class="leb-smallgathering-detail-img-right"
+        >
           <span id="leb-smallgathering-detail-limit-title">신청 제한</span>
           <span id="leb-smallgathering-detail-limit-gender">
             <span class="leb-smallgathering-detail-title">성별</span>
-            <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_gender_limit }}</span>
+            <span class="leb-smallgathering-detail-content">{{
+              gatheringVo.small_gathering_gender_limit
+            }}</span>
           </span>
           <span id="leb-smallgathering-detail-limit-age">
             <span class="leb-smallgathering-detail-title">나이</span>
-            <span class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_age_limit }}</span>
+            <span class="leb-smallgathering-detail-content">{{
+              gatheringVo.small_gathering_age_limit
+            }}</span>
           </span>
         </div>
 
@@ -95,16 +157,16 @@
           <div id="leb-smallgathering-detail-course-location">코스 위치</div>
           <div id="MGDKMmap">
             <div style="display: none"></div>
-
           </div>
         </div>
 
         <!--모임정보-->
         <div id="leb-smallgathering-detail-information">
           <div class="leb-smallgathering-detail-title">모임정보</div>
-          <div class="leb-smallgathering-detail-content">{{ gatheringVo.small_gathering_information }}</div>
+          <div class="leb-smallgathering-detail-content">
+            {{ gatheringVo.small_gathering_information }}
+          </div>
         </div>
-
       </div>
     </div>
     <AppFooter />
@@ -116,12 +178,10 @@
 import "@/assets/css/LebCss/SmallGatheringDetail.css";
 import AppFooter from "@/components/AppFooter.vue";
 import AppHeader from "@/components/AppHeader.vue";
-import axios from 'axios';
-
-
+import axios from "axios";
 
 export default {
-  name: 'SmallGatheringDetailView',
+  name: "SmallGatheringDetailView",
   components: {
     AppFooter,
     AppHeader,
@@ -130,6 +190,10 @@ export default {
   data() {
     return {
       file: "",
+      applicationVo: {
+        small_gathering_no: "",
+        users_no: "",
+      },
       gatheringVo: {
         users_no: "",
         course_no: "",
@@ -147,6 +211,7 @@ export default {
         small_gathering_age_limit: "",
         small_gathering_saveName: "",
         application_no_count: "",
+        is_application: "",
       },
       course_point_Vo: {
         course_point_no: "",
@@ -157,7 +222,7 @@ export default {
         course_division: "",
         group_num: "",
       },
-      course_point_List: []
+      course_point_List: [],
     };
   },
   mounted() {
@@ -179,7 +244,6 @@ export default {
   },
   methods: {
     initMap() {
-
       this.course_point_Vo.course_no = this.$route.params.course_no;
       var mapContainer = document.getElementById("MGDKMmap"), // 지도를 표시할 div
         mapOption = {
@@ -283,39 +347,111 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-
     },
     /*
     callChildMethod(course_no) {
       this.$refs.pjhref.initMap(course_no);
     },*/
+    // 해당 소모임 정보
     getSmallGatheringDetailData() {
       axios({
-        method: 'get', // put, post, delete                   
-        url: `${this.$store.state.apiBaseUrl}/api/walking/getSmallGatheringDetailData/` + this.$route.params.small_gathering_no,
-        headers: { "Content-Type": "application/json; charset=utf-8" },//전송타입+토큰
+        method: "get", // put, post, delete
+        url:
+          `${this.$store.state.apiBaseUrl}/api/walking/getSmallGatheringDetailData/` +
+          this.$route.params.small_gathering_no,
+        headers: { "Content-Type": "application/json; charset=utf-8",
+          "Authorization": "Bearer " + this.$store.state.token
+        }, //전송타입+토큰
 
         //params: guestbookVo, //get방식 파라미터로 값이 전달
         //data: guestbookVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
 
-        responseType: 'json' //수신타입
-      }).then(response => {
-        //console.log(response.data.apiData);//수신데이타
-        
-        this.gatheringVo = response.data.apiData
-        //console.log(this.gatheringVo);//수신데이타
-        
-        //this.$refs.pjhref.initMap(this.gatheringVo.course_no);
-        //this.callChildMethod(this.gatheringVo.course_no);
-        //console.log(this.gatheringVo.course_no);
-        //this.initMap();
-        //console.log("===========================================");
+        responseType: "json", //수신타입
+      })
+        .then((response) => {
+          //console.log(response.data.apiData);//수신데이타
 
+          this.gatheringVo = response.data.apiData;
+          console.log(this.gatheringVo);//수신데이타
 
-      }).catch(error => {
-        console.log(error);
-      });
+          //this.$refs.pjhref.initMap(this.gatheringVo.course_no);
+          //this.callChildMethod(this.gatheringVo.course_no);
+          //console.log(this.gatheringVo.course_no);
+          //this.initMap();
+          //console.log("===========================================");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
+    // 소모임 삭제
+    SmallGatheringDelete() {
+      console.log("삭제");
+
+      axios({
+        method: "delete", // put, post, delete
+        url: `${this.$store.state.apiBaseUrl}/api/walking/smallgatheringupdatedelete`,
+        headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+        // params: guestbookVo, //get방식 파라미터로 값이 전달
+        data: this.gatheringVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+        responseType: "json", //수신타입
+      })
+        .then((response) => {
+          console.log(response.data); //수신데이타
+          this.$router.push("/walking/smallgatheringpage");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    // 소모임 신청 등록
+    applicationUpdate() {
+      console.log("소모임 신청");
+      this.applicationVo.small_gathering_no = this.gatheringVo.small_gathering_no;
+      this.applicationVo.users_no = this.$store.state.authUser.users_no;
+
+      axios({
+        method: "post", // put, post, delete
+        url: `${this.$store.state.apiBaseUrl}/api/walking/applicationupdatedelete`,
+        headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+        // params: guestbookVo, //get방식 파라미터로 값이 전달
+        data: this.applicationVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+        responseType: "json", //수신타입
+      })
+        .then((response) => {
+          console.log(response.data); //수신데이타
+          this.getSmallGatheringDetailData();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    // 소모임 신청 취소
+    applicationDelete() {
+      console.log("소모임 취소");
+      this.applicationVo.small_gathering_no = this.gatheringVo.small_gathering_no;
+      this.applicationVo.users_no = this.$store.state.authUser.users_no;
+
+      axios({
+        method: "delete", // put, post, delete
+        url: `${this.$store.state.apiBaseUrl}/api/walking/applicationupdatedelete`,
+        headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+        // params: guestbookVo, //get방식 파라미터로 값이 전달
+        data: this.applicationVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+        responseType: "json", //수신타입
+      })
+        .then((response) => {
+          console.log(response.data); //수신데이타
+          this.getSmallGatheringDetailData();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
   },
   created() {
     this.getSmallGatheringDetailData();
