efab6b51a4eaa29fbdce1178ab0fe15f91d7a25d
seed-yoo <lukdown10@naver.com>
2024-06-11 18:02:07 +0900

    yys

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -66,9 +66,163 @@ export default {
   name: "AppHeader",
   components: {},
   data() {
-    return {};
+    return {
+      users_InfoVo: {
+        users_no: '',
+        users_kcal: '',
+        users_length: '',
+      },
+    };
   },
   methods: {
+    // 로그인 유저 정보
+    getUsers_Info() {
+      console.log("데이터 가져오기");
+
+      axios({
+        method: "get", 
+        url: `${this.$store.state.apiBaseUrl}/api/walking/mypage_users_info`,
+        headers: { "Content-Type": "application/json; charset=utf-8",
+                    Authorization: "Bearer " + this.$store.state.token
+         }, 
+        responseType: "json", 
+      }).then((response) => {
+          //console.log(response.data.apiData); 
+          this.users_InfoVo = response.data.apiData;
+          console.log(this.users_InfoVo);
+
+          
+          if(this.users_InfoVo.users_length >= 30000 && this.users_InfoVo.users_length < 50000) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 1) {
+                this.challengeUpdate(1);
+              }
+            }
+          } 
+          if(this.users_InfoVo.users_length < 30000 && this.users_InfoVo.users_length >= 20000) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 2) {
+                this.challengeUpdate(2);
+              }
+            }
+          }
+          if(this.users_InfoVo.users_no != null) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 3) {
+                this.challengeUpdate(3);
+              }
+            }
+          }
+          if(this.users_InfoVo.small_group_count >= 100) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 4) {
+                this.challengeUpdate(4);
+              }
+            }
+          }
+          if(this.users_InfoVo.course_users_count >= 15) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 5) {
+                this.challengeUpdate(5);
+              }
+            }
+          }
+          if(this.users_InfoVo.course_users_sum_hit >= 100000) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 6) {
+                this.challengeUpdate(6);
+              }
+            }
+          }
+          if(this.users_InfoVo.review_users_count >= 100) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 7) {
+                this.challengeUpdate(7);
+              }
+            }
+          }
+          if(this.users_InfoVo.gallery_users_count >= 100) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 8) {
+                this.challengeUpdate(8);
+              }
+            }
+          }
+          if(this.users_InfoVo.users_length >= 50000) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 9) {
+                this.challengeUpdate(9);
+              }
+            }
+          } 
+          if(this.users_InfoVo.users_kcal >= 30000) {
+            for (let index = 0; index < this.challengeList.length; index++) {
+              this.getchallengeList();
+              if(this.challengeList[index].is_challenge_t_f == 0 && this.challengeList[index].challenge_no == 10) {
+                this.challengeUpdate(10);
+              }
+            }
+          }
+
+          this.getChallenge_Complete();
+          
+         
+
+        }).catch((error) => {
+              console.log(error);
+          });
+    },
+    // 개인 도전 과제 등록
+    challengeUpdate(challenge_no) {
+          console.log("개인 도전 과제 저장");
+          this.challengeVo.users_no = this.$store.state.authUser.users_no;
+          this.challengeVo.challenge_no = challenge_no;
+          //console.log(this.challengeVo);
+          axios({
+            method: "post", // put, post, delete
+            url: `${this.$store.state.apiBaseUrl}/api/walking/challengeupdate`,
+            headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+            // params: guestbookVo, //get방식 파라미터로 값이 전달
+            data: this.challengeVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+            responseType: "json", //수신타입
+          }).then((response) => {
+              console.log(response.data); //수신데이타
+              
+          }).catch((error) => {
+              console.log(error);
+          });
+        },
+
+        // 과제 달성 정보
+        getChallenge_Complete() {
+          console.log("데이터 가져오기");
+
+          axios({
+            method: "get", 
+            url: `${this.$store.state.apiBaseUrl}/api/walking/mypage_challenge_complete`,
+            headers: { "Content-Type": "application/json; charset=utf-8",
+                        Authorization: "Bearer " + this.$store.state.token
+            }, 
+            responseType: "json", 
+          }).then((response) => {
+              //console.log(response.data.apiData); 
+              this.challenge_complete = response.data.apiData;
+              console.log(this.challenge_complete);
+
+            }).catch((error) => {
+                  console.log(error);
+              });
+        },
     courseAlert() {
       alert("로그인을 해주세요");
     },
@@ -160,6 +314,9 @@ export default {
     },
   },
 
-  created() { },
+  created() { 
+    this.getUsers_Info();
+    this.getChallenge_Complete();
+  },
 };
 </script>
